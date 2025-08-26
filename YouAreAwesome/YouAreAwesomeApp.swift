25afc95e0a7e23d2b8443def3f6284d82e5d4b56
Timohty Petrik <timmypetrik123456789@gmail.com>
2025-08-26 08:25:04 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Timothy Petrik on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
