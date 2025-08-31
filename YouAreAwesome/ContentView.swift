dcd2e5d15b586c19b0979f46b399c7513ecd93d9
Timohty Petrik <timmypetrik123456789@gmail.com>
2025-08-31 15:12:27 -0400

    Added I am a developper

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You Are Awesome!")
+            Text("I am a Devlopper!")
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundStyle(.red)
