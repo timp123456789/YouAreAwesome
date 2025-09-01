2b66403e8e938cdf1aba9dd180e9bfc03da2883f
Timohty Petrik <timmypetrik123456789@gmail.com>
2025-08-31 20:15:29 -0400

    Button

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I am a Programmer"
     var body: some View {
         
         VStack {
@@ -15,10 +16,13 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("I am a Developer!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundStyle(.red)
+            Button("Click Me!") {
+                message = "Awesome!"
+            }
         }
         .padding()
         
